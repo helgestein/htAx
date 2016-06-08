0cdaeb7f1cb9d27bebbb4ba93568429bb00696cf
Sally Jiao <jiao.sally@gmail.com>
2016-06-08 08:46:08 +0200

    "edited getTernCoord.m to clean up code"

--- getTernCoord.m
+++ getTernCoord.m
@@ -1,6 +1,7 @@
+% converts the percentages of A and B into rectangular coordinates for a
+% ternary diagram
+
 function [xCoord, yCoord] = getTernCoord(fracA, fracB, sqrt3Half, sqrt3Inv)
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
     yCoord = fracB * sqrt3Half;
     xCoord = fracA + yCoord * sqrt3Inv;
 end
