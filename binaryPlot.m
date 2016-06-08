4c5005b088af045f3abd8d18dda2cb6712969327
Helge Stein <helge.stein@rub.de>
2016-06-08 18:21:02 +0200

    added comments changed style

--- binaryPlot.m
+++ binaryPlot.m
@@ -5,18 +5,20 @@ function [ output_args ] = binaryPlot( xAxis, yAxis, composition,scaling)
     %   yAxis: a Matrix containing the intensities for the selected XRD pattern
     %   composition: the composition by which the xrd pattern should be sorted
     %   scaling: choose between linear, sqrt, and log10 scaling
-    [sComposition,ID]=sort(composition); %sort them according to the composition
-    [x,y] = meshgrid(xAxis, sComposition);
-    if (scaling==2)
-        yAxis=sqrt(yAxis);
-    end 
-    if (scaling==3)
-        yAxis=log10(yAxis);
-    end 
-    surf(x,y,yAxis(:,ID).')
-    xlabel('Anlge'); ylabel('Composition');
-    shading interp; axis tight;
-    view(2);
+    [sComposition,ID] = sort(composition); %sort them according to the composition
+    [x,y] = meshgrid(xAxis, sComposition); %create what is nessesary for surf plot
     
+    %select the scaling function
+    if (scaling == 2)
+        yAxis = sqrt(yAxis);
+    end 
+    if (scaling == 3)
+        yAxis = log10(yAxis);
+    end 
+    
+    surf(x,y,yAxis(:,ID).')%plot the data
+    xlabel('Anlge'); ylabel('Composition');
+    shading interp; axis tight; %this is to make it look good 
+    view(2); %this is so ensure the correct view
 end
 
