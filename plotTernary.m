b04469b52b896c7cbedae470e43d0bdabbdb0bdf
Sally Jiao <jiao.sally@gmail.com>
2016-06-08 08:34:31 +0200

    "edited plotTernary.m to clean up code"

--- plotTernary.m
+++ plotTernary.m
@@ -1,6 +1,7 @@
-function [ output_args ] = plotTernary(compA, compB)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+% creates a ternary diagram from compositions of A, B, and (implied) C,
+% given by arrays compA and compB
+
+function [] = plotTernary(compA, compB)
 
 % plot settings
 numTicks = 5;
@@ -14,16 +15,13 @@ plot([0 1 0.5 0], [0 0 sqrt3Half 0], 'k');
 
 hold on;
 
-% plot grid
+% plot gridlines
 tickBottom = linspace(0, 1, numTicks + 1);
 tickBottom = tickBottom(1:numTicks);
-
 for i = 1:numTicks
     [xTickLeft(i), yTickLeft(i)] = getTernCoord(0, 1 - tickBottom(i), sqrt3Half, sqrt3Inv);
     [xTickRight(i), yTickRight(i)] = getTernCoord(1 - tickBottom(i), tickBottom(i), sqrt3Half, sqrt3Inv);
 end
-
-% plot constant A lines
 for i = 2:numTicks
     partnerTick = numTicks - i + 2;
     plot([tickBottom(i) xTickRight(partnerTick)], [0 yTickRight(partnerTick)], 'k'); % constant A
@@ -31,8 +29,6 @@ for i = 2:numTicks
     plot([xTickLeft(i) tickBottom(partnerTick)], [yTickLeft(i) 0], 'k'); % constant C
 end
     
-%labels = num2str(majorticks'*100);
-
 % get rectangular coordinates from compositions and plot points
 numPoints = length(compA);
 for i = 1:numPoints
