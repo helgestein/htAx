6491f7e80b0c3eeecc294b6a2ebc3a26051c4514
Sally Jiao <jiao.sally@gmail.com>
2016-06-09 15:14:31 +0200

    split into two windows + bug fix

--- binaryPlot.m
+++ binaryPlot.m
@@ -1,4 +1,4 @@
-function [ output_args ] = binaryPlot( xAxis, yAxis, composition,scaling, handle)
+function [ output_args ] = binaryPlot( xAxis, yAxis, composition,scaling, axesHandle)
     %BINARYPLOT plots data onto a binary plot
     %   plots data on a nice binary plot where the x axis and y axis are nicely
     %   labeled. xAxis:  a vector containing e.g. the Angles of a XRD pattern
@@ -8,9 +8,6 @@ function [ output_args ] = binaryPlot( xAxis, yAxis, composition,scaling, handle
     [sComposition,ID] = sort(composition); %sort them according to the composition
     [x,y] = meshgrid(xAxis, sComposition); %create what is nessesary for surf plot
     
-    cla(handle);
-    
-    %select the scaling function
     if (scaling == 2)
         yAxis = sqrt(yAxis);
     end 
@@ -18,9 +15,12 @@ function [ output_args ] = binaryPlot( xAxis, yAxis, composition,scaling, handle
         yAxis = log10(yAxis);
     end 
     
-    surf(handle, x,y,yAxis(:,ID).')%plot the data
-    xlabel('Angle'); ylabel('Composition');
-    shading interp; axis tight; %this is to make it look good 
-    view(2); %this is so ensure the correct view
+    surf(axesHandle, x,y,yAxis(:,ID).'); %plot the data
+    xlabel(axesHandle, 'Angle'); 
+    ylabel(axesHandle, 'Composition');
+    shading(axesHandle, 'interp'); 
+    axis(axesHandle, 'tight'); %this is to make it look good 
+    view(axesHandle, 2);
+    %view(2); %this is so ensure the correct view
 end
 
