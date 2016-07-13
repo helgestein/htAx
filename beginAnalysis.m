875b8dcadbfe021a4f39f2a0b305c14fdebdedb6
Sally Jiao <jiao.sally@gmail.com>
2016-07-13 14:42:10 +0200

    "xrd pattern bar plot"

--- beginAnalysis.m
+++ beginAnalysis.m
@@ -1,5 +1,5 @@
 function [] = beginAnalysis(XRDFolder, EDXFile, saveFile, ...
-    ECFolder, XRDDatabaseFolder)
+    ECFolder, XRDDatabaseFolder, filenameInfo)
 %BEGINANALYSIS takes in the name of a folder that contains XRD data, the
 %name of a file with EDX data, and the name of a file to which the analysis
 %can be saved and begins a new analysis session with the data
@@ -8,53 +8,57 @@ function [] = beginAnalysis(XRDFolder, EDXFile, saveFile, ...
 %   '/Users/sjiao/Documents/summer_2016/data/MnFeCoO-EDX',
 %   '/Users/sjiao/Documents/summer_2016/code/testFiles/testSave.txt')
 
-% import and read XRD and EDX data
-[xCoord, yCoord, XRDData] = readXRDData(XRDFolder);
-[A, B, C] = ...
-    importEDXFile(EDXFile);
+    % read in XRD and EDX data
+    [xCoord, yCoord, XRDData] = readXRDData(XRDFolder, filenameInfo);
+    [A, B, C] = importEDXFile(EDXFile);
 
-% convert EDX data to percents
-A = A ./ 100;
-B = B ./ 100;
-C = C ./ 100;
+    % convert EDX data to percents
+    A = A ./ 100;
+    B = B ./ 100;
+    C = C ./ 100;
 
-% FOR TEST FILE ONLY remove first five rows of EDX data
-%{
-rowsToRemove = 5;
-lengthNew = length(A) - rowsToRemove;
-ATemp = zeros(lengthNew, 1);
-BTemp = zeros(lengthNew, 1);
-CTemp = zeros(lengthNew, 1);
-for i = (rowsToRemove + 1):length(A)
-    ATemp(i - rowsToRemove) = A(i);
-    BTemp(i - rowsToRemove) = B(i);
-    CTemp(i - rowsToRemove) = C(i);
-end    
-A = ATemp;
-B = BTemp;
-C = CTemp;
-%}
+    % read in EC data
+    if ECFolder ~= 1
+        ECData = readECData(ECFolder, xCoord, yCoord, filenameInfo);
+    else
+        ECData = 1;
+    end
 
-% read in EC data
-if ECFolder ~= 1
-    ECData = readECData(ECFolder, xCoord, yCoord);
-else
-    ECData = 1;
-end
+    % read in XRD database folder
+    if XRDDatabaseFolder ~= 1
+        [collcodes, XRDDatabase] = readXRDDatabase(XRDDatabaseFolder);
+    else
+        collcodes = 1;
+        XRDDatabase = 1;
+    end
+    
+    %[XRDData(:, 459), XRDData(:, 460)]
+    
+    %find all points for which XRD data was not taken
+    ids = find(XRDData(1, :) == 0);
+    %testids = find(XRDData(1, 2* (1:(size(XRDData, 2)/2))) == 0)
+    %XRDData(10, ids)
+    %XRDData(12, ids)
+    %ids
+    %XRDData(1, 658)
+    numToRemove = length(ids);
+    edxToRemove = ids(2 * (1:(numToRemove / 2))) / 2;
+    A = removerows(A, edxToRemove);
+    B = removerows(B, edxToRemove);
+    C = removerows(C, edxToRemove);
+    %length(A)
+    %colsToRemove = [ids * 2 - 1 ids * 2];
+    colsToRemove = ids;
+    XRDData = transpose(removerows(transpose(XRDData), colsToRemove));
+    ECData = transpose(removerows(transpose(ECData), colsToRemove));
+    %size(XRDData, 2) / 2
+    %size(ECData, 2) / 2
 
-if XRDDatabaseFolder ~= 1
-    [collcodes, XRDDatabase] = readXRDDatabase(XRDDatabaseFolder);
-else
-    collcodes = 1;
-    XRDDatabase = 1;
-end
+    pointInfo = zeros(1, 11);
+    numSelected = 0;
+    ECPlotInfo = zeros(342, 4);
 
-pointInfo = zeros(1, 11);
-numSelected = 0;
-ECPlotInfo = zeros(342, 4);
-
-openFigs(saveFile, XRDData, A, B, C, numSelected, pointInfo, ECData, ...
-    ECPlotInfo, collcodes, XRDDatabase);
-
+    openFigs(saveFile, XRDData, A, B, C, numSelected, pointInfo, ECData, ...
+        ECPlotInfo, collcodes, XRDDatabase);
 end
 
